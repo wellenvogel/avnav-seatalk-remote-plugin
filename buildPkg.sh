81b16c63b5c67d5e387e04935a05fbbd346d3d7d
andreas <andreas@wellenvogel.de>
2021-01-16 17:06:14 +0100

    switch to our normal versioning schema

--- buildPkg.sh
+++ buildPkg.sh
@@ -6,14 +6,14 @@ pdir=`readlink -f "$pdir"`
 config=package.yaml
 version="$1"
 if [ "$version" = "" ] ; then
-  version=`date '+%Y.%-m.%-d'`
+  version=`date '+%Y%m%d'`
 fi
 echo building version $version
 tmpf=package$$.yaml
 rm -f $tmpf
 sed "s/^ *version:.*/version: \"$version\"/" $config > $tmpf
 config=$tmpf
-docker run  --rm   -v "$pdir":/tmp/pkg   --user `id -u`:`id -g` -w /tmp/pkg goreleaser/nfpm pkg -p deb -f $config
+docker run  --rm   -v "$pdir":/tmp/pkg   --user `id -u`:`id -g` -w /tmp/pkg wellenvogel/nfpm:1.0 pkg -p deb -f $config
 rt=$?
 if [ "$tmpf" != "" ] ; then
   rm -f $tmpf
